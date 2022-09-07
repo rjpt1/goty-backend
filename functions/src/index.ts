0b213c29cbba15a49b99ce79f61460ce09ee0b6f
rafael-pedraza <rjpt1@outlook.com>
2022-09-06 20:34:42 -0500

    correcciones de sugerencia del  lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,14 @@
 import * as functions from "firebase-functions";
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
 
-import * as express from 'express';
-import * as cors from 'cors';
+import * as express from "express";
+import * as cors from "cors";
 
 const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://firestore-grafica-73b21-default-rtdb.firebaseio.com"
-  });
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firestore-grafica-73b21-default-rtdb.firebaseio.com",
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -20,18 +20,15 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 });
 
 const db = admin.firestore();
-export const getgoty = functions.https.onRequest(async(request, response) => {
-
+export const getgoty = functions.https.onRequest( async (request, response) => {
   // const nombre = request.query.nombre || 'Sin nombre';
   // response.json({
-  //   nombre  
+  //   nombre
   // });
-
-  const gotyRef = db.collection('goty');
+  const gotyRef = db.collection("goty");
   const docsSnap = await gotyRef.get();
-  const juegos = docsSnap.docs.map( doc => doc.data() );
+  const juegos = docsSnap.docs.map( (doc) => doc.data() );
   response.json(juegos);
-
 });
 
 // express
@@ -39,34 +36,33 @@ const app = express();
 
 app.use(cors({origin: true}));
 
-app.get('/goty', async(req, res) => {
-  const gotyRef = db.collection('goty');
+app.get("/goty", async (req, res) => {
+  const gotyRef = db.collection("goty");
   const docsSnap = await gotyRef.get();
-  const juegos = docsSnap.docs.map( doc => doc.data() );
+  const juegos = docsSnap.docs.map( (doc) => doc.data() );
   res.json(juegos);
 });
 
-app.post('/goty/:id', async(req, res) => {
+app.post("/goty/:id", async (req, res) => {
   const id = req.params.id;
-  const gameRef = db.collection('goty').doc(id);
+  const gameRef = db.collection("goty").doc(id);
   const gameSnap = await gameRef.get();
 
   if (!gameSnap.exists) {
     res.status(404).json({
       ok: false,
-      mensaje: 'No existe un juego con ese id' + id
+      mensaje: "No existe un juego con ese id" + id,
     });
-  }
-  else {
-    res.json('Juego existe');
+  } else {
+    res.json("Juego existe");
 
-    const antes =  gameSnap.data() || {votos: 0 };
-    await   gameRef.update({
-      votos: antes.votos + 1
+    const antes = gameSnap.data() || {votos: 0};
+    await gameRef.update({
+      votos: antes.votos + 1,
     });
     res.json({
       ok: true,
-      mensaje: `Gracias por tu voto a ${antes.name}`
+      mensaje: `Gracias por tu voto a ${antes.name}`,
     });
   }
 });
